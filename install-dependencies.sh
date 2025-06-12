9f57c9e5356c44db9a089e3274ee2a98d42d9bb9
KRITVI SHARMA <kritvisharma0912@gmail.com>
2025-06-12 09:59:53 +0400

    dependency script added

---
+++ install-dependencies.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+
+
+sudo apt-get -y install python3-pip python3-opencv python3-pyqt5 libxml2-dev libxslt-dev
+pip3 install --upgrade pip
+pip3 install ultralytics supervision pymavlink numpy imutils
+sudo apt-get -y install python3-libcamera
+pip3 install picamera2
